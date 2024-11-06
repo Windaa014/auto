f0f6780d50e6af2eed8ff2d379fc93da13af56e2
Windaa014 <oyoy1413@gmail.com>
2024-11-06 12:24:14 +0700

    windahosting

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ check_token() {
   echo -e "${YELLOW}MASUKAN AKSES TOKEN :${NC}"
   read -r USER_TOKEN
 
-  if [ "$USER_TOKEN" = "freebywindhost" ]; then
+  if [ "$USER_TOKEN" = "windaslebew" ]; then
     echo -e "${GREEN}AKSES BERHASIL${NC}}"
   else
     echo -e "${GREEN}Selamat datang di auto install by windhost${NC}"
